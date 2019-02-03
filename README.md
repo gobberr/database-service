f61eaaea7c0874450ef9884fe040d001a9cf1a8d
gobberr <rupert.gobber@yahoo.it>
2019-02-03 17:56:03 +0100

    Adding routes

--- README.md
+++ README.md
@@ -1,33 +1,23 @@
-# ISDEproject
 
-This web service offers one-day planning. Using the Google Calendar and EasyRoom APIs, it returns the merge of the events with the free classrooms in which to spend the free time. Check it out! 
-
-## https://isde-project.herokuapp.com
-
-APIs:
-
-1.  Google +
-
-2.  Google Calendar
-
-3.  EasyRoom
-
-4.  Mongodb
-
-
-## Scenario: 
-
-1.	Sign in with google accounts using Google+ APIs
-
-2.  Selecting a google calendar to get your events	
-
-3. 	Merge of events in the calendar with free classrooms of University of Trento
-
+## Deployment url
+## https://database-service-isde.herokuapp.com
 
 ## Setup:
 
-1. Install the dependencies: `npm install`
+1.  Install the dependencies 
+    `npm install`
 
-2. Start the service `npm start`
+2.  Start the service 
+    `npm start`
 
-3. Go to `localhost:3000` and follow the instructions
+3.  The service is running on port 
+    `3002`
+
+## APIS:
+
+1. `https://database-service.herokuapp.com/get-events`  - get events for user
+
+    Parameters
+    *   
+
+2. `https://database-service.herokuapp.com/get-day`  - get events in this day for user
--- routes/router.js
+++ routes/router.js
@@ -9,29 +9,32 @@ const Day = require('../models/day-model');
 
 //----------------------------------------------------------------------------------------
 
-router.get('/', function(req, res, next) {    
-  res.send('Homepage')
+router.get('/', function(req, res, next) {      
+  res.send('Waiting for request...')
 });
 
 // get Events for user
 router.get('/get-events', function(req, res, next) {
+  console.log('called API get-events')
   Events.find( { googleId: req.user.googleId } , function(err, events) {      
     if(err) console.log('Error retrieving data from mongo');
     res.send( events );  
-  });  
+  });
 });
 
 // get Day for user
 router.get('/get-day', function(req, res, next) {     
+  console.log('called API get-day')
   let currentDate = time.getCurrentDate();
-  Day.find({ googleId: req.user.googleId, date: currentDate }, function(err, events) {
+  Day.find({ googleId: req.form.googleId, date: currentDate }, function(err, events) {
     if(err) console.log(err)
     res.send( events );
   })
 });
 
 router.post('/delete-events', function (req, res, next) { 
-  Events.deleteMany({ googleId: req.user.googleId }, function(err, events) {
+  console.log('called API delete-events')
+  Events.deleteMany({ googleId: req.qs.googleId }, function(err, events) {
     if(err) console.log('Error deleting data from mongo');
     res.send( events );  
   });
